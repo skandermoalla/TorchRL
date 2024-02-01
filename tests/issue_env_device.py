91063bbb03a5ddcdf0f3aa624f0de9f0b31ea1d7
Skander Moalla <skan.moalla@gmail.com>
2024-02-01 18:40:32 +0000

    env device issue.

--- tests/issue_env_device.py
+++ tests/issue_env_device.py
@@ -4,7 +4,8 @@ from torchrl.envs import (
     EnvCreator,
     ExplorationType,
     StepCounter,
-    TransformedEnv, SerialEnv,
+    TransformedEnv,
+    SerialEnv,
 )
 from torchrl.envs.libs.gym import GymEnv
 from torchrl.modules import OneHotCategorical, ProbabilisticActor
@@ -18,8 +19,9 @@ device = "cuda:0"
 def build_cpu_single_env():
     env = GymEnv(env_id, device="cpu")
     env = TransformedEnv(env)
-    env.append_transform(StepCounter(max_steps=max_step, step_count_key="single_env_step_count", truncated_key="single_env_truncated"))
+    env.append_transform(StepCounter(max_steps=max_step))
     return env
+
 
 def build_actor(env):
     return ProbabilisticActor(
@@ -34,6 +36,7 @@ def build_actor(env):
         default_interaction_type=ExplorationType.RANDOM,
     )
 
+
 if __name__ == "__main__":
     env = SerialEnv(n_env, EnvCreator(lambda: build_cpu_single_env()), device=device)
     policy_module = build_actor(env)
@@ -42,7 +45,7 @@ if __name__ == "__main__":
 
     for i in range(100):
         batches = env.rollout((max_step + 3), policy=policy_module, break_when_any_done=False)
-        max_step_count = batches["next", "single_env_step_count"].max().item()
+        max_step_count = batches["next", "step_count"].max().item()
         if max_step_count > max_step:
             print("Problem!")
             print(max_step_count)
---
+++ tests/issue_truncated_device.py
@@ -0,0 +1,55 @@
+from tensordict.nn import TensorDictModule
+from torch import nn
+from torchrl.envs import ExplorationType, SerialEnv
+from torchrl.collectors import SyncDataCollector
+from torchrl.envs import RewardSum, StepCounter, TransformedEnv, ParallelEnv, EnvCreator
+from torchrl.envs.libs.gym import GymEnv
+from torchrl.modules import ProbabilisticActor, OneHotCategorical
+
+N = 202
+env_id = "MountainCar-v0"
+device = "cuda:0"
+
+
+def build_single_env():
+    env = GymEnv(env_id)
+    env = TransformedEnv(env)
+    env.append_transform(StepCounter(max_steps=N))
+    return env
+
+
+if __name__ == "__main__":
+    env = ParallelEnv(4, EnvCreator(lambda: build_single_env()), device=device)
+    env = TransformedEnv(env)
+
+    policy_module = ProbabilisticActor(
+        module=TensorDictModule(
+            nn.LazyLinear(env.action_spec.space.n, device=device),
+            in_keys=["observation"],
+            out_keys=["logits"],
+        ),
+        spec=env.action_spec,
+        distribution_class=OneHotCategorical,
+        in_keys=["logits"],  # in_keys of the distribution.
+        default_interaction_type=ExplorationType.RANDOM,
+    )
+    policy_module(env.reset())
+
+    collector = SyncDataCollector(
+        env,
+        policy=policy_module,
+        frames_per_batch=N * 4,
+        total_frames=100 * N * 4,
+        reset_at_each_iter=True,
+        device=device,
+    )
+    max_step_count = 200
+    for batches in collector:
+        batches = env.rollout(N)
+        max_step_count = batches["next", "step_count"].max().item()
+        if max_step_count > 200:
+            print("Problem!")
+            print(max_step_count)
+            break
+    else:
+        print("No problem!")
