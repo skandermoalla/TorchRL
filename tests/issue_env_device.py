a9b17bd3dc6714e37cba5269d92ea63cdcd10c05
Skander Moalla <skan.moalla@gmail.com>
2024-02-01 17:03:37 +0000

    env device issue.

--- tests/issue_env_device.py
+++ tests/issue_env_device.py
@@ -4,61 +4,61 @@ from torchrl.collectors import SyncDataCollector
 from torchrl.envs import (
     EnvCreator,
     ExplorationType,
-    ParallelEnv,
-    RewardSum,
     StepCounter,
-    TransformedEnv,
+    TransformedEnv, SerialEnv,
 )
 from torchrl.envs.libs.gym import GymEnv
 from torchrl.modules import OneHotCategorical, ProbabilisticActor
 
-N = 202
-env_id = "MountainCar-v0"
-device = "mps"  # "cuda:0
+max_step = 200
+n_env = 4
+env_id = "CartPole-v1"
+device = "cuda:0"
 
 
-def build_single_env():
-    env = GymEnv(env_id)
+def build_cpu_single_env():
+    env = GymEnv(env_id, device="cpu")
     env = TransformedEnv(env)
-    env.append_transform(StepCounter(max_steps=N, truncated_key="truncated_sc"))
+    env.append_transform(StepCounter(max_steps=max_step, step_count_key="single_env_step_count", truncated_key="single_env_truncated"))
     return env
 
-
-if __name__ == "__main__":
-    env = ParallelEnv(4, EnvCreator(lambda: build_single_env()), device=device)
-    env = TransformedEnv(env)
-    env.append_transform(RewardSum())
-
-    policy_module = ProbabilisticActor(
+def build_actor(env):
+    return ProbabilisticActor(
         module=TensorDictModule(
-            nn.LazyLinear(env.action_spec.space.n, device=device),
+            nn.LazyLinear(env.action_spec.space.n),
             in_keys=["observation"],
             out_keys=["logits"],
         ),
         spec=env.action_spec,
         distribution_class=OneHotCategorical,
-        in_keys=["logits"],  # in_keys of the distribution.
+        in_keys=["logits"],
         default_interaction_type=ExplorationType.RANDOM,
     )
+
+if __name__ == "__main__":
+    env = SerialEnv(n_env, EnvCreator(lambda: build_cpu_single_env()), device=device)
+    env = TransformedEnv(env)
+
+    policy_module = build_actor(env)
+    policy_module.to(device)
+    dummy_t = env.reset()
+    policy_module(dummy_t)
 
     collector = SyncDataCollector(
         env,
         policy=policy_module,
-        frames_per_batch=N * 4,
-        total_frames=100 * N * 4,
+        frames_per_batch=(max_step+3) * n_env,
+        total_frames=100 * (max_step+3) * n_env,
         reset_at_each_iter=True,
+        device=device,
     )
-    max_step_count = 200
+    max_step_count = max_step
     for batches in collector:
-        best_return = batches["next", "episode_reward"][batches["next", "done"]].max().item()
-        max_step_count = batches["next", "step_count"].max().item()
-        if max_step_count > 200:
-            print("Problem!")
-            print(best_return, max_step_count)
-            break
+        max_step_count = batches["next", "single_env_step_count"].max().item()
 
-    print(f'done: {batches["next", "done"].squeeze(-1).nonzero().squeeze(-1)}')
-    print(f'truncated: {batches["next", "truncated"].squeeze(-1).nonzero().squeeze(-1)}')
-    print(f'truncated_sc: {batches["next", "truncated_sc"].squeeze(-1).nonzero().squeeze(-1)}')
-    print(f'terminated: {batches["next", "terminated"].squeeze(-1).nonzero().squeeze(-1)}')
-    pass
+        if max_step_count > max_step:
+            print("Problem!")
+            print(max_step_count)
+            break
+    else:
+        print("No problem!")
